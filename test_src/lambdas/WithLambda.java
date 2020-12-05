2f5b8e59b15caad40f4a6bb22b05007f9e0858b3
Nicolas Anquetil <nicolas.anquetil@inria.fr>
2020-12-05 12:47:05 +0100

    Clarified testLambda

--- test_src/lambdas/WithLambda.java
+++ test_src/lambdas/WithLambda.java
@@ -6,16 +6,21 @@ import java.util.Collection;
 
 public class WithLambda {
 
-    private Set<String> segments = new TreeSet<>((String seg1, String seg2) -> {
+  private Set<String> segments =
+    new TreeSet<>(
+      (String seg1, String seg2) -> {
         if (seg1.equals(seg2)) {
-            return 0;
-        } else {
-            return 1;
-        }
-    });
+    	  return 0;
+    	} else {
+    	  return 1;
+    	}
+      }
+    );
 
-    public WithLambda() {
-        Collection<Object> col;
-        boolean found = col.stream().anyMatch(t -> System.out.print("lambda!"));
-    }
+  public WithLambda() {
+    Collection<Object> col;
+    boolean found =
+      col.stream()
+        .anyMatch( t -> System.out.print("lambda!") );
+  }
 }
--- tests/fr/inria/verveine/extractor/java/VerveineJTest_Lambdas.java
+++ tests/fr/inria/verveine/extractor/java/VerveineJTest_Lambdas.java
@@ -3,9 +3,11 @@ package fr.inria.verveine.extractor.java;
 import org.junit.Before;
 import org.junit.Test;
 import org.moosetechnology.model.famixjava.famixjavaentities.Access;
+import org.moosetechnology.model.famixjava.famixjavaentities.Lambda;
 import org.moosetechnology.model.famixjava.famixjavaentities.LocalVariable;
 import org.moosetechnology.model.famixjava.famixjavaentities.Method;
 import org.moosetechnology.model.famixjava.famixtraits.TLocalVariable;
+import org.moosetechnology.model.famixjava.famixtraits.TNamedEntity;
 
 import java.io.File;
 import java.util.Collection;
@@ -31,11 +33,11 @@ public class VerveineJTest_Lambdas extends VerveineJTest_Basic {
 	private void parse(String[] sources) {
 		parser.configure( sources);
 		parser.parse();
-		parser.emitMSE(VerveineJOptions.OUTPUT_FILE);
+		//parser.emitMSE(VerveineJOptions.OUTPUT_FILE);
 	}
 
     @Test
-    public void testLambdaTypedParameter() {
+    public void testLambdaTypedParametersAreVariablesLocalToParentMethod() {
         parse(new String[] {"-alllocals", "test_src/lambdas"});
 
         Method meth = detectFamixElement( Method.class, JavaDictionary.INIT_BLOCK_NAME);
@@ -61,7 +63,7 @@ public class VerveineJTest_Lambdas extends VerveineJTest_Basic {
     }
 
     @Test
-    public void testLambdaUnTypedParameter() {
+    public void testLambdaUntypedParameterIsVariableLocalToParentMethod() {
         parse(new String[] {"-alllocals", "test_src/lambdas"});
 
         Method meth = detectFamixElement( Method.class, "WithLambda");
@@ -70,6 +72,7 @@ public class VerveineJTest_Lambdas extends VerveineJTest_Basic {
         LocalVariable col = null;
         LocalVariable found = null;
         LocalVariable t = null;
+
         assertEquals(3, meth.getLocalVariables().size());
         for (TLocalVariable tlvar : meth.getLocalVariables()) {
             LocalVariable lvar = (LocalVariable) tlvar;
@@ -87,12 +90,17 @@ public class VerveineJTest_Lambdas extends VerveineJTest_Basic {
             }
         }
         assertNotNull(col);
+        assertNotNull(col.getDeclaredType());
+
         assertNotNull(found);
+        assertNotNull(found.getDeclaredType());
+        
         assertNotNull(t);
+        assertNull(t.getDeclaredType());
     }
 
     @Test
-    public void testLambdaNotAllLocals() {
+    public void testLambdaUntypedParameterNotCreatedWithoutOptionAlllocals() {
         parse(new String[] {"test_src/lambdas"});
 
         Method meth = detectFamixElement( Method.class, "WithLambda");
@@ -102,17 +110,32 @@ public class VerveineJTest_Lambdas extends VerveineJTest_Basic {
         LocalVariable col = (LocalVariable) firstElt(meth.getLocalVariables());
         assertNotNull(col);
         assertEquals("col", col.getName());
-
-        assertEquals(1, meth.getAccesses().size()); // access to "out" variable of System
     }
 
     @Test
-    public void testAccessInDeclaration(){
+    public void testAccesses(){
         parse(new String[] {"-alllocals", "-anchor", "assoc", "test_src/lambdas"});
         Collection<Access> accesses = entitiesOfType(Access.class);
         assertEquals(6, accesses.size());
 
-
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "segments", /*writeAccess*/true));
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "seg1", /*writeAccess*/false));
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "seg2", /*writeAccess*/false));
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "found", /*writeAccess*/true));
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "col", /*writeAccess*/false));
+        accesses.stream().anyMatch( a -> accessToVariableWriting(a, "out", /*writeAccess*/false));
     }
 
+    /** Checks that Access is on variable names varName and whether it is a writeAccess or not
+     */
+	protected boolean accessToVariableWriting(Access access, String varName, boolean writeAccess) {
+		if (access.getIsWrite() != writeAccess) return false;
+		return ((TNamedEntity)access.getVariable()).getName().equals(varName);
+	}
+
+	@Test
+	public void testLambdasDeclarations() {
+		Collection<Lambda> lambdas = entitiesOfType(Lambda.class);
+		assertEquals(2, lambdas.size());
+	}
 }
