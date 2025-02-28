f98cb040ca96a70a51184f5e59e219a6eb029269
Soufyane Labsari <soufyane.labsari@berger-levrault.com>
2025-02-28 11:23:37 +0100

    Fix #107 set the isStub attribute on implicit variables

--- app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
+++ app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
@@ -2857,6 +2857,7 @@ public class EntityDictionary {
 		ImplicitVariable fmx;
 		fmx = ensureFamixEntity(ImplicitVariable.class, key, name);
 		fmx.setParentBehaviouralEntity(tMethod);
+		fmx.setIsStub(tMethod.getIsStub());
 		return fmx;
 	}
 
---
+++ app/src/test/resources/ad_hoc/ImplicitVarExamples.java
@@ -0,0 +1,12 @@
+package ad_hoc;
+
+public class ImplicitVarExamples {
+
+	public void methodWithThis() {
+		System.out.println(this.toString());
+	}
+	
+	public void methodWithSuper() {
+		System.out.println(super.toString());
+	}
+}
