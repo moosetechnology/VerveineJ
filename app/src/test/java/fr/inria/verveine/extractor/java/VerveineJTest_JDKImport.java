01930068b2f38260fd475ef669a37e680db65534
Romain <romain.degrave@inria.fr>
2025-12-12 16:16:39 +0100

    Adding an option to parse JDKs (with a JDK version < 9)

---
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_JDKImport.java
@@ -0,0 +1,140 @@
+package fr.inria.verveine.extractor.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+import org.eclipse.jdt.internal.compiler.env.IModule;
+import org.eclipse.jdt.internal.compiler.batch.FileSystem.Classpath;
+import org.eclipse.jdt.internal.compiler.lookup.TypeConstants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.moosetechnology.model.famix.famixjavaentities.Class;
+import org.moosetechnology.model.famix.famixjavaentities.IndexedFileAnchor;
+import org.moosetechnology.model.famix.famixtraits.TInheritance;
+import org.moosetechnology.model.famix.famixtraits.TInvocable;
+import org.moosetechnology.model.famix.famixtraits.TInvocation;
+import org.moosetechnology.model.famix.famixtraits.TMethod;
+
+public class VerveineJTest_JDKImport extends VerveineJTest_Basic {
+
+	@Before
+	public void setUp() throws Exception {
+		new File(DEFAULT_OUTPUT_FILE).delete();
+		parser = new VerveineJParser();
+		repo = parser.getFamixRepo();
+
+		OutputStreamWriter writer = new FileWriter("resources/java/base/module-info.java");
+		writer.write("module java.base {}");
+		writer.close();
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		new File(("resources/java/base/module-info.java")).delete();
+	}
+
+	@Test
+	public void testReferenceToJavaLangClassIsStubWithoutJDK() {
+
+		parser.configure(new String[] { "resources/javalanguser" });
+		parser.parse();
+
+		Class clazz = detectFamixElement(Class.class, "Toto");
+		TMethod[] methods = clazz.getMethods().toArray(new TMethod[0]);
+		assertEquals(1, methods.length);
+		TInvocation[] outgoingInvocations = methods[0].getOutgoingInvocations().toArray(new TInvocation[0]);
+		TInvocable[] candidates = outgoingInvocations[0].getCandidates().toArray(new TInvocable[0]);
+		Class declaredType = (Class) ((TMethod) candidates[0]).getParentType();
+		assertTrue(declaredType.getIsStub());
+	}
+
+	@Test
+	public void testLoadOurJDKStringClass() {
+
+		parser.configure(new String[] { "resources/java/lang/String.java" });
+		parser.parse();
+
+		Class ourString = detectFamixElement(Class.class, "String");
+		assertFalse(ourString.getIsStub());
+		assertEquals(((IndexedFileAnchor) ourString.getSourceAnchor()).getFileName(),
+				"resources/java/lang/String.java");
+		assertEquals(1, entitiesNamed(Class.class, "String").size());
+	}
+
+	@Test
+	public void testLoadOurJDKObjectClass() {
+
+		parser.configure(new String[] { "resources/java/lang/Object.java" });
+		parser.parse();
+
+		assertEquals(1, entitiesNamed(Class.class, "Object").size());
+		Class ourObject = detectFamixElement(Class.class, "Object");
+		assertFalse(ourObject.getIsStub());
+		assertEquals(((IndexedFileAnchor) ourObject.getSourceAnchor()).getFileName(),
+				"resources/java/lang/Object.java");
+	}
+
+	@Test
+	public void testHasRefToExternalClassLoadingStringFirst() {
+
+		parser.configure(new String[] { "-jdkMode", "-1.7", "resources/java/lang/String.java",
+				"resources/java/lang/Object.java", "resources/javalanguser/Toto.java" });
+		parser.parse();
+
+		assertEquals(1, entitiesNamed(Class.class, "String").size());
+		Class ourString = detectFamixElement(Class.class, "String");
+
+		Class clazz = detectFamixElement(Class.class, "Toto");
+		TMethod[] methods = clazz.getMethods().toArray(new TMethod[0]);
+		assertEquals(1, methods.length);
+		TInvocation[] outgoingInvocations = methods[0].getOutgoingInvocations().toArray(new TInvocation[0]);
+		TInvocable[] candidates = outgoingInvocations[0].getCandidates().toArray(new TInvocable[0]);
+		Class declaredType = (Class) ((TMethod) candidates[0]).getParentType();
+		assertEquals(declaredType, ourString);
+	}
+
+	@Test
+	public void testHasRefToExternalClassLoadingStringSecond() {
+
+		parser.configure(new String[] { "-jdkMode", "-1.7", "resources/javalanguser/Toto.java",
+				"resources/java/lang/String.java", "resources/java/lang/Object.java" });
+		parser.parse();
+
+		assertEquals(1, entitiesNamed(Class.class, "String").size());
+		Class ourString = detectFamixElement(Class.class, "String");
+
+		Class clazz = detectFamixElement(Class.class, "Toto");
+		TMethod[] methods = clazz.getMethods().toArray(new TMethod[0]);
+		assertEquals(1, methods.length);
+		TInvocation[] outgoingInvocations = methods[0].getOutgoingInvocations().toArray(new TInvocation[0]);
+		TInvocable[] candidates = outgoingInvocations[0].getCandidates().toArray(new TInvocable[0]);
+		Class declaredType = (Class) ((TMethod) candidates[0]).getParentType();
+		assertEquals(declaredType, ourString);
+	}
+
+	@Test
+	public void testHasSuperclassRefToOurObject() {
+
+		parser.configure(new String[] { "-jdkMode", "-1.7", "resources/java/lang/String.java",
+				"resources/java/lang/Object.java", "resources/javalanguser/Toto.java" });
+		parser.parse();
+
+		Class ourObject = detectFamixElement(Class.class, "Object");
+
+		Class clazz = detectFamixElement(Class.class, "Toto");
+		TInheritance[] inheritances = clazz.getSuperInheritances().toArray(new TInheritance[0]);
+		assertEquals(1, inheritances.length);
+		Class superclass = (Class) inheritances[0].getSuperclass();
+		assertEquals(superclass, ourObject);
+	}
+}
