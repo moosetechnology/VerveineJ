0bb267e0fe7d5833879dee8e3960f9342053117a
Romain <romain.degrave@inria.fr>
2026-04-02 15:35:14 +0200

    Add an option to choose the runtime library used for bindings by JDT

--- app/src/main/java/fr/inria/verveine/extractor/java/VerveineJOptions.java
+++ app/src/main/java/fr/inria/verveine/extractor/java/VerveineJOptions.java
@@ -134,6 +134,14 @@ public class VerveineJOptions {
 	 * Am I parsing a JDK?
 	 */
 	protected boolean parsingJdk = false;
+	
+	/**
+	 * Path to system library for JDT to use 
+	 * (for Java version <= 8 <=> path to rt.jar)
+	 * (for Java version > 8 <=> path to jrt-fs.jar? have not try so to verify in practice)
+	 */
+	protected String pathToSystemLibrary;
+	
 
 	public VerveineJOptions() {
 		this.allLocals = false;
@@ -211,7 +219,7 @@ public class VerveineJOptions {
 			usage();
 			System.exit(0);
 		}
-		else if (arg.matches("-1\\.[1-7]") || arg.matches("-[1-7]")) {
+		else if (arg.matches("-1\\.[1-8]") || arg.matches("-[1-8]")) {
 			setCodeVersion(arg);
 		} else if (arg.equals("-alllocals")) {
 			allLocals = true;
@@ -244,6 +252,13 @@ public class VerveineJOptions {
 				argumentsTreated++;
 			} else {
 				throw new IllegalArgumentException("-o requires a filename");
+			}
+		} else if (arg.equals("-sysLibPath")) {
+			if (i < args.length) {
+				pathToSystemLibrary = args[i+1].trim();
+				argumentsTreated++;
+			} else {
+				throw new IllegalArgumentException("-sysLibPath requires a filename");
 			}
 		} else if (arg.equals("-i")) {
 			incrementalParsing = true;
@@ -395,6 +410,8 @@ public class VerveineJOptions {
 		System.err.println("      [-excludepath GLOBBINGEXPR] A globbing expression of file path to exclude from parsing");
 		System.err.println("      [-1.1 | -1 | -1.2 | -2 | ... | -1.7 | -7] specifies version of Java");
 		System.err.println("      [-jdkMode] option to ABSOLUTELY set if you are making a model of a JDK");
+		System.err.println("      [-sysLibPath <path-to-runtime-jar>] path to the system library for JDT to resolve native Java binding in the parsed code");
+		System.err.println("      									  By default, JDT will collect the library used to execute VVJ (unless the option -jdkMode is active).");
 		System.err.println("      <files-to-parse>|<dirs-to-parse> list of source files to parse or directories to search for source files");
 	}
 
@@ -417,7 +434,9 @@ public class VerveineJOptions {
 			codeVers = JavaCore.VERSION_1_6;
 		} else if (arg.equals("-1.7") || arg.equals("-7")) {
 			codeVers = JavaCore.VERSION_1_7;
-		}
+		} else if (arg.equals("-1.8") || arg.equals("-8")) {
+			codeVers = JavaCore.VERSION_1_8;
+		} 
 	
 	}
 
@@ -426,10 +445,19 @@ public class VerveineJOptions {
 	}
 
 	public void configureJDTParser(ASTParser jdtParser) {
-		// If I am parsing a JDK, jdt must not provide the VM's running libraries (=jdk used by VerveineJ at runtime)
-		boolean includeRunningVMBootclasspath = !parsingJdk;
+		// If I am parsing a JDK or providing the system library,
+		// JDT must not fetch the VM's running libraries (=JRE used by VerveineJ at runtime)
+		boolean includeRunningVMBootclasspath = !(parsingJdk || pathToSystemLibrary != null);
 		
-		jdtParser.setEnvironment(classPathOptions, /*sourcepathEntries*/argPath.toArray(new String[0]), /*encodings*/null, includeRunningVMBootclasspath);
+		if (pathToSystemLibrary != null) {
+			List<String> deps = new ArrayList<String>();
+			deps.add(pathToSystemLibrary);
+			classPathOptions = addToClassPath(classPathOptions, deps);
+		}
+		
+		String[] sourcePathEntries = argPath.toArray(new String[0]);
+		
+		jdtParser.setEnvironment(classPathOptions, /*sourcepathEntries*/sourcePathEntries, /*encodings*/null, includeRunningVMBootclasspath);
 		jdtParser.setResolveBindings(true);
 		/**
 		 *  Incremental parsing should not activate Binding recovery because using this option with incremental parsing
@@ -440,13 +468,19 @@ public class VerveineJOptions {
 		}
 		jdtParser.setKind(ASTParser.K_COMPILATION_UNIT);
 		
-		Map<String, String> javaCoreOptions = JavaCore.getOptions();
-
-		javaCoreOptions.put(JavaCore.COMPILER_COMPLIANCE, codeVers);
-		javaCoreOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, codeVers);
-		javaCoreOptions.put(JavaCore.COMPILER_SOURCE, codeVers);
+		if (codeVers != null) {
+			Map<String, String> javaCoreOptions = JavaCore.getOptions();
 	
-		jdtParser.setCompilerOptions(javaCoreOptions);
+			javaCoreOptions.put(JavaCore.COMPILER_COMPLIANCE, codeVers);
+			javaCoreOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, codeVers);
+			javaCoreOptions.put(JavaCore.COMPILER_SOURCE, codeVers);
+			
+			// Not absolutely sure it is necessary
+			if (pathToSystemLibrary != null)
+				javaCoreOptions.put(JavaCore.COMPILER_PB_ENABLE_PREVIEW_FEATURES, JavaCore.DISABLED);
+		
+			jdtParser.setCompilerOptions(javaCoreOptions);
+		}
 
 	}
 
