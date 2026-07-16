f14936695571564488a47e2622ca481b17311ebf
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-16 14:36:32 +0200

    test to get duplications on ArrayList

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -32,16 +32,20 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 	}
 
 	@Test
-	public void testArrayListConstructorShouldNotBeDupplicated() {
+	public void testArrayListConstructorEntityTypingShouldNotBeDupplicatedWithVoid() {
 		parse(new String[] { "src/test/resources/entity_typing/FastArrayList.java",
 				"src/test/resources/entity_typing/PriorityQueue.java",
 				"src/test/resources/entity_typing/StaticBucketMap.java" });
 
 		// looking for java.util.ArrayList.ArrayList() duplication in all classes
 		Method arrayListConstructor = null;
+		int voidTypingCount = 0;
 		String target = "ArrayList";
 		String targetMethod = "ArrayList()";
+		String targetType = "void";
+		
 
+		//Get the arrayList constructor
 		for (Method method : entitiesOfType(Method.class)) {
 
 			String methodName = method.getName();
@@ -51,9 +55,17 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 			if ((methodName.equals(target)) && className.equals(targetMethod)) {
 				arrayListConstructor = method;
 				break;
-			}
+			} 
 		}
 
 		assertNotNull(arrayListConstructor);
+		
+		for(EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			if(typing.getTypedEntity() == arrayListConstructor && typing.getDeclaredType() != null && targetType.equals(typing.getDeclaredType().getName())){
+				voidTypingCount++;
+			}
+		}
+		
+		assertEquals(1, voidTypingCount);
 	}
 }
