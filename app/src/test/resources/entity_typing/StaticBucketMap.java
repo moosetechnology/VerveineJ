d110bca48e7b0b6be644f1c7778e01fe7ba5085f
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-17 16:48:31 +0200

    more minimisation

--- app/src/test/resources/entity_typing/StaticBucketMap.java
+++ app/src/test/resources/entity_typing/StaticBucketMap.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
 
 public final class StaticBucketMap {
-
-    private class EntryIterator implements Iterator {
-        private ArrayList current = new ArrayList();
-    }
+	private ArrayList current = new ArrayList();
 }
