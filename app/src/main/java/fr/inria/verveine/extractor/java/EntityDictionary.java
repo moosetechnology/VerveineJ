47a4cfd27af7eecb2c0080348aeb4c534d41caf7
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-20 15:36:38 +0200

    using our new attribute that represent the option

--- app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
+++ app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
@@ -862,8 +862,8 @@ public class EntityDictionary {
 	public Class ensureFamixClassStubOwner() {
 		Class fmx =  ensureFamixUniqEntity(Class.class, null, STUB_METHOD_CONTAINER_NAME);
 		if (fmx != null) {
-			/*if the option `useMissingPackage` is activated, we put all the stubs with unknown packages in a package `Missing` that will regroup them in a same place*/
-			if(this.options != null && this.options.isUseMissingPackage()) {
+			/*if the option `useMissingPackage` is activated, we put all the stubs with unknown packages in a package `Missing*/
+			if(this.useMissingPackage) {
 				fmx.setTypeContainer(ensureFamixPackageMissing());
 			}
 			/*otherwise we put them in the default package*/
