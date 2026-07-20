f3f706be47688e46750e1d5ad9908a809d0627da
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-20 10:01:21 +0200

    minor

--- app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
+++ app/src/main/java/fr/inria/verveine/extractor/java/EntityDictionary.java
@@ -862,7 +862,7 @@ public class EntityDictionary {
 	public Class ensureFamixClassStubOwner() {
 		Class fmx =  ensureFamixUniqEntity(Class.class, null, STUB_METHOD_CONTAINER_NAME);
 		if (fmx != null) {
-			/*if the option isClassifyStubs is on, we put all the stubs with miss binding in a package `Missing` that will regroup them in a same place*/
+			/*if the option isClassifyStubs is activated, we put all the stubs with unknown packages in a package `Missing` that will regroup them in a same place*/
 			if(this.options != null && this.options.isClassifyStubs()) {
 				fmx.setTypeContainer(ensureFamixPackageMissing());
 			}
