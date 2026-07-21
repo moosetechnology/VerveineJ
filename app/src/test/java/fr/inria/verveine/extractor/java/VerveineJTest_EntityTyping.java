9eb4feebd3b7fbb18d8a35d52ee5e8d561df56b2
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-21 15:55:52 +0200

    adding a test to check that the fix don't unify entities from different package

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import org.moosetechnology.model.famix.famixjavaentities.EntityTyping;
 import org.moosetechnology.model.famix.famixjavaentities.Method;
+import org.moosetechnology.model.famix.famixjavaentities.Class;
 
 public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 
@@ -63,5 +65,45 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 		
 		// We should not have any duplication of entity typing on void
 		assertEquals(1, voidCount);
+	}
+	
+	@Test
+	public void testNoUnificationForSameNameInDifferentPackages() {
+		parse(new String[] { 
+				"src/test/resources/entity_typing/packageA/SameName.java",
+				"src/test/resources/entity_typing/packageB/SameName.java"
+		 });
+
+		String targetClass = "SameName";
+		String targetMethod = "doSomething";
+		String targetMethodSignature = "doSomething()";
+		List<Class> targetClasses = new ArrayList<Class>();
+		List<Method> targetMethods = new ArrayList<Method>();
+		
+		// get the targetted classes
+		for (Class actualClass : entitiesOfType(Class.class)) {
+			if (targetClass.equals(actualClass.getName())) {
+				targetClasses.add(actualClass);
+			}
+		}
+
+		// get the targetted methods
+		for (Method method : entitiesOfType(Method.class)) {
+			if (targetMethod.equals(method.getName()) && targetMethodSignature.equals(method.getSignature())) {
+				targetMethods.add(method);
+			}
+		}
+
+		// We should have exactly 2 classes and 2 methods, not unified into one
+		assertEquals(2, targetClasses.size());
+		assertEquals(2, targetMethods.size());
+
+		// check that they belong to different packages
+		Class class1 = targetClasses.get(0);
+		Class class2 = targetClasses.get(1);
+		
+		assertNotNull(class1.getTypeContainer());
+		assertNotNull(class2.getTypeContainer());
+		assertFalse(class1.getTypeContainer().equals(class2.getTypeContainer()));
 	}
 }
