9e84b74c36370c48035622f961e4eb3e9d0d9177
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-28 11:20:39 +0200

    updating tests for entity typing

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -1,5 +1,39 @@
 package fr.inria.verveine.extractor.java;
 
-public class VerveineJTest_EntityTyping {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.moosetechnology.model.famix.famixjavaentities.EntityTyping;
+import org.moosetechnology.model.famix.famixjavaentities.Method;
+import org.moosetechnology.model.famix.famixjavaentities.Class;
+
+public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
+
+	protected VerveineJParser parser;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		new File(DEFAULT_OUTPUT_FILE).delete();
+		parser = new VerveineJParser();
+		repo = parser.getFamixRepo();
+	}
+
+	private void parse(String[] sources) {
+		parser.configure(sources);
+		parser.parse();
+		parser.exportModel(DEFAULT_OUTPUT_FILE);
+	}
+
 
 }
