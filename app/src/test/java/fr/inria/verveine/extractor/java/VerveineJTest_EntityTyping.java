1829093c69041848e22ff651117b009222870703
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-29 11:26:44 +0200

    adding a test to test that the future fix will not break the initial logic

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -3,6 +3,7 @@ package fr.inria.verveine.extractor.java;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -37,16 +38,18 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 	}
 
 	/*
-	 * This test is here due to some bug in VVJ that create EntityTyping that's link
-	 * a null entity to a void
+	 * These tests are here due to some bug in VVJ that create 2 EntityTyping on
+	 * methods with void type a normal one, and a bugged one that just type null
+	 * with a void type
 	 */
 	@Test
 	public void testEntityTypingDontHaveNullTypedEntity() {
-		parse(new String[] { "src/test/resources/entity_typing/ZipOutputStream.java" });
+		parse(new String[] { "src/test/resources/entity_typing/PhantomVoidTyping.java" });
 
 		int nullEntityCount = 0;
 
 		for (EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			// if type is void
 			if (typing.getDeclaredType() != null && "void".equals(typing.getDeclaredType().getName())) {
 				if (typing.getTypedEntity() == null) {
 					nullEntityCount++;
@@ -56,5 +59,40 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 
 		// we should not have null entities linked to void
 		assertEquals(0, nullEntityCount);
+	}
+
+	@Test
+	public void testEntityTypingForVoidMethodsAreCorrectlyLinked() {
+		parse(new String[] { "src/test/resources/entity_typing/PhantomVoidTyping.java" });
+
+		int totalVoidTyping = 0;
+		int nullEntityCount = 0;
+		List<String> typedMethodName = new ArrayList<String>();
+
+		for (EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			// if type is void
+			if (typing.getDeclaredType() != null && "void".equals(typing.getDeclaredType().getName())) {
+				totalVoidTyping++;
+
+				//null entities
+				if (typing.getTypedEntity() == null) {
+					nullEntityCount++;
+				}
+				
+				//well typed entity
+				else if (typing.getTypedEntity() instanceof Method) {
+					 typedMethodName.add(((Method) typing.getTypedEntity()).getName());
+				}
+			}
+		}
+		// 0 phantoms and 3 entity typing(void) for the 3 methods
+		assertEquals(0, nullEntityCount);
+		
+		assertTrue(typedMethodName.contains("myVoidMethod"));
+		assertTrue(typedMethodName.contains("myVoidMethod2"));
+		assertTrue(typedMethodName.contains("myVoidMethod3"));
+		
+		assertEquals(3, totalVoidTyping);
+		
 	}
 }
