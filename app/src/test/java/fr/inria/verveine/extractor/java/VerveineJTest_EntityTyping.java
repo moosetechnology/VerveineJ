38625c14984847d2cebfe0421cb27989d2f4f054
Antonin Goslin <antonin.goslin62@gmail.com>
2026-07-30 11:12:30 +0200

    adding test for the primitive types, different than void

--- app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
+++ app/src/test/java/fr/inria/verveine/extractor/java/VerveineJTest_EntityTyping.java
@@ -38,11 +38,13 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 	}
 
 	/*
-	 * These tests are here due to some bug in VVJ that create 2 EntityTyping on methods with void return type.
-	 * A normal one that type well the entity, and a bugged one that just link null and void
+	 * These tests are here due to some bug in VVJ that creates 2 EntityTyping on
+	 * methods with void return type. A normal one that type well the entity, and a
+	 * bugged one that just link null and void We also test the fix on primitives
+	 * types
 	 */
 	@Test
-	public void testEntityTypingDontHaveNullTypedEntity() {
+	public void testEntityTypingDontHaveNullTypedEntityOnVoidType() {
 		parse(new String[] { "src/test/resources/entity_typing/PhantomVoidTyping.java" });
 
 		int nullEntityCount = 0;
@@ -56,6 +58,23 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 			}
 		}
 
+		// we should not have null entities linked to void
+		assertEquals(0, nullEntityCount);
+	}
+	
+	@Test
+	public void testEntityTypingDontHaveNullTypedEntityOnPrimitiveTypes() {
+		parse(new String[] { "src/test/resources/entity_typing/PhantomMethodTyping.java" });
+
+		int nullEntityCount = 0;
+
+		for (EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			// check primitives types
+			if (typing.getTypedEntity() == null) {
+				nullEntityCount++;
+			}
+		}
+		
 		// we should not have null entities linked to void
 		assertEquals(0, nullEntityCount);
 	}
@@ -73,25 +92,66 @@ public class VerveineJTest_EntityTyping extends VerveineJTestAbstract {
 			if (typing.getDeclaredType() != null && "void".equals(typing.getDeclaredType().getName())) {
 				totalVoidTyping++;
 
-				//null entities
+				// null entities
 				if (typing.getTypedEntity() == null) {
 					nullEntityCount++;
 				}
-				
-				//well typed entity
+
+				// well typed entity
 				else if (typing.getTypedEntity() instanceof Method) {
-					 typedMethodName.add(((Method) typing.getTypedEntity()).getName());
+					typedMethodName.add(((Method) typing.getTypedEntity()).getName());
 				}
 			}
 		}
 		// 0 phantoms and 3 entity typing(void) for the 3 methods
 		assertEquals(0, nullEntityCount);
-		
+
 		assertTrue(typedMethodName.contains("myVoidMethod"));
 		assertTrue(typedMethodName.contains("myVoidMethod2"));
 		assertTrue(typedMethodName.contains("myVoidMethod3"));
-		
+
 		assertEquals(3, totalVoidTyping);
-		
+
+	}
+	
+	@Test
+	public void testEntityTypingForNonVoidMethodsAreCorrectlyLinked() {
+		parse(new String[] { "src/test/resources/entity_typing/PhantomMethodTyping.java" });
+
+		int totalStringTyping = 0;
+		int totalIntTyping = 0;
+		int nullEntityCount = 0;
+		List<String> typedMethodName = new ArrayList<String>();
+
+		for (EntityTyping typing : entitiesOfType(EntityTyping.class)) {
+			if (typing.getDeclaredType() != null) {
+				String typeName = typing.getDeclaredType().getName();
+				
+				// get string and int
+				if ("String".equals(typeName) || "int".equals(typeName)) {
+					
+					if ("String".equals(typeName)) totalStringTyping++;
+					if ("int".equals(typeName)) totalIntTyping++;
+
+					// null entities
+					if (typing.getTypedEntity() == null) {
+						nullEntityCount++;
+					}
+
+					// well typed entity
+					else if (typing.getTypedEntity() instanceof Method) {
+						typedMethodName.add(((Method) typing.getTypedEntity()).getName());
+					}
+				}
+			}
+		}
+		// 0 phantoms and 2 entity typing, 1 int and 1 string
+		assertEquals(0, nullEntityCount);
+
+		assertTrue(typedMethodName.contains("myStringMethod"));
+		assertTrue(typedMethodName.contains("myIntMethod"));
+
+		assertEquals(1, totalIntTyping);
+		assertEquals(1, totalStringTyping);
 	}
 }
