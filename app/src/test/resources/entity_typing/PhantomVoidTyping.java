262186256ee3cd09aab5fdd1db9e970ec38b4a24
Antonin Goslin <antonin.goslin62@gmail.com>
2026-08-12 11:28:04 +0200

    renaming tests resources

--- app/src/test/resources/entity_typing/PhantomVoidTyping.java
+++
@@ -1,9 +0,0 @@
-/**
- * This file is here to reproduce a bug that create entity typing on null entities
- * With the bug, every void generate one entity typing linked to the method, and one to null
- */
-public class PhantomVoidTyping {
-    public void myVoidMethod() {}
-    public void myVoidMethod2() {}
-    public void myVoidMethod3() {}
-}
